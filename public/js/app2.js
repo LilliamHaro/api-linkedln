59c3c59cdba3d2d32a7706b318f965f9639ea4bd
LilliamHaro <qualia950@gmail.com>
2018-02-14 22:17:56 -0500

    corrigiendo funcionalidad

--- public/js/app2.js
+++ public/js/app2.js
@@ -1,3 +1,14 @@
-let userName = window.localStorage.getItem('user-name');
-
-document.write('bienvenido '+ userName);
+window.addEventListener('load', function() {
+  let userName = window.localStorage.getItem('user-name');
+  let headline = window.localStorage.getItem('headline');
+  let url = window.localStorage.getItem('url');
+  $('#user').text('Welcome '+ userName);
+  $('#headline').text(headline);
+  $('#url').attr('href',url)
+  $('#btn').on('click', function() {
+    IN.User.logout(logout);
+    function logout() {
+      window.location.href = '../index.html';
+    }
+  })
+});
